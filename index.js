8e8c7a185d87d138635611538bb074474fd92b2a
Nagender Singh Shekhawat <nagendersinghh@gmail.com>
2025-12-14 18:56:05 +0530

    Add formsubmit endpoint

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const form = document.querySelector("#enquire-info");
 
 async function onSubmit() {
-    const queryString = new URLSearchParams(new FormData(form)).toString();
+    const data = JSON.stringify(Object.fromEntries(new FormData(form)))
+
+    const ackElem = document.getElementById("acknowledgement")
+    ackElem.style.display = "block"
+    setTimeout(() => {
+        if(ackElem && ackElem.style && ackElem.style.display) {
+            ackElem.style.display = "none"
+        }
+    }, 3000)
+
     try {
-        const response = await fetch("/enquire", {
+        const response = await fetch("https://formsubmit.co/ajax/d0ddb6224e25de5049d4306cd64ea5f", {
             method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: queryString
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: data
         })
-        console.log("response", response)
-        form.reset()
     } catch (e) {
         console.error(e)
     }
+    form.reset()
 }
 
 form.addEventListener("submit", event => {
